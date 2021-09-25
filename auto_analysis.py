fb7276f0cb4910d5c623c4a1637549739b75735b
Jumpei Kajimoto <jay0423@i.softbank.jp>
2021-09-25 17:34:08 +0900

    test

--- auto_analysis.py
+++ auto_analysis.py
@@ -5,26 +5,27 @@ from ansys.mapdl.core import launch_mapdl
 class AutoAnalysis:
 
     mapdl = None
+    N = 3
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self._num = 0
 
 
     def setup(self):
         self.mapdl = launch_mapdl()
-        time.spleep(1)
-        self.mapdl.cwd(r"C:\Users\matlab\ansys_kajimoto\test1")
-        self.mapdl.filname("tset1", key=1)
+        time.sleep(1)
+        self.mapdl.cwd(r"C:\Users\matlab\ansys_kajimoto\test2")
+        self.mapdl.filname("tset{}".format(self._num), key=1)
 
     
     def analysis(self):
         path = r"C:\Users\matlab\Documents\ansys-management\etc\sample_test\test1.ansys"
         self.mapdl.input(path)
         time.sleep(5)
-        print("解析スタート")
+        print("解析{} スタート".format(self._num))
         self.mapdl.solve()
         self.mapdl.finish()
-        print("finish")
+        print("finish：解析{}".format(self._num))
 
 
     
@@ -32,12 +33,16 @@ class AutoAnalysis:
         self.mapdl.post26()
         self.mapdl.rforce(2, "NNUM", "F", "X", "FX")
         text = self.mapdl.prvar(2)
-        output_path = "test.csv"
+        output_path = "test{}.csv".format(self._num)
         with open(output_path, 'w') as f:
             f.write(text)
     
 
     def main(self):
-        self.setup()
-        self.analysis()
-        self.output()
+        for _ in range(self.N):
+            self._num += 1
+            self.setup()
+            self.analysis()
+            self.output()
+            self.mapdl.exit()
+            time.sleep(5)
