df8715347124861394bcd70bd5a7132e5fa09c7e
Jumpei Kajimoto <jay0423@i.softbank.jp>
2021-10-26 20:56:55 +0900

    update

---
+++ start.py
@@ -0,0 +1,5 @@
+import shutil
+
+shutil.copy('ansys_management/start/ansys_management.txt', './ansys_management.py')
+shutil.copy('ansys_management/start/settings_child.txt', './settings_child.py')
+shutil.copy('ansys_management/start/path.xlsx', './path.xlsx')
